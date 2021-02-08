b909fa947119e895b360db3a634cf31be6f24b30
martos <martos@strands.com>
2021-02-08 14:51:32 +0100

    1. Clarity of code [Unit tests refactor - Gilded Rose] - Refactor Default Item tests (Test 2)

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemRefactoredTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemRefactoredTest.java
@@ -9,15 +9,8 @@ public class GildedRoseADefaultItemRefactoredTest {
 	private static final int NOT_EXPIRED_SELLIN = 15;
 	private static final int DEFAULT_QUALITY = 3;
 	private static final String DEFAULT_ITEM = "DEFAULT_ITEM";
+	private static final int EXPIRED_SELLIN = -1;
 
-	/**
-	 * Method to test the variation in quality of the item for the non expired
-	 * Item.
-	 * 
-	 * The quality should decrease by 1 when the item is not expired
-	 * and sell in should decrease by 1.
-	 * 
-	 */
 	@Test
 	public void unexpiredDefaultItem_qualityDecreasesBy1() {
 		// GIVEN
@@ -29,13 +22,21 @@ public class GildedRoseADefaultItemRefactoredTest {
 		// THEN
 		Item expected = new Item(DEFAULT_ITEM, NOT_EXPIRED_SELLIN - 1, DEFAULT_QUALITY - 1);
 
-		assertItem(app.items[0], expected);
+		assertItem(expected, app.items[0]);
 	}
 
-	private void assertItem(Item actual, Item expected) {
-		assertEquals(expected.name, actual.name);
-		assertEquals(expected.sellIn, actual.sellIn);
-		assertEquals(expected.sellIn, actual.quality);
+	@Test
+	public void expiredDefaultItem_qualityDecreasesBy2() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(DEFAULT_ITEM, EXPIRED_SELLIN, DEFAULT_QUALITY);
+
+		// THEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(DEFAULT_ITEM, EXPIRED_SELLIN - 1, DEFAULT_QUALITY - 2);
+
+		assertItem(expected, app.items[0]);
 	}
 
 	private GildedRose createGildedRoseWithOneItem(String defaultItem, int notExpiredSellin, int defaultQuality) {
@@ -44,19 +45,10 @@ public class GildedRoseADefaultItemRefactoredTest {
 		return new GildedRose(items);
 	}
 
-	/**
-	 * Method to test the variation in quality of the item for the non expired
-	 * Item.
-	 * 
-	 * The quality should decrease by 2 when the item is expired(Sell in  < 0) and sell in should decrease by 1.
-	 * 
-	 */
-	@Test
-	public void testUpdateQualityForExpiredItem() {
-		GildedRose app = createGildedRoseWithOneItem("DEFAULT_ITEM", -1, 3);
-		app.updateQuality();
-		assertEquals("DEFAULT_ITEM", app.items[0].name);
-		assertEquals(-2, app.items[0].sellIn);
-		assertEquals(1, app.items[0].quality);
+	private void assertItem(Item expected, Item actual) {
+		assertEquals(expected.name, actual.name);
+		assertEquals(expected.sellIn, actual.sellIn);
+		assertEquals(expected.quality, actual.quality);
 	}
+
 }
