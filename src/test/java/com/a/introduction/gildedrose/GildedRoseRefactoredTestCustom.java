cd1f7ec15d8db302264f7d0a8161703247f731a8
martos <martos@strands.com>
2021-02-08 16:29:11 +0100

    1. Clarity of code [Unit tests refactor - Gilded Rose] - Refactor BackStagePasses tests (Test 1, 2 and 3)

--- src/test/java/com/a/introduction/gildedrose/GildedRoseRefactoredTestCustom.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseRefactoredTestCustom.java
@@ -4,21 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GildedRoseADefaultItemRefactoredTest {
+public class GildedRoseRefactoredTestCustom {
 
-	private static final int NOT_EXPIRED_SELLIN = 15;
-	private static final int DEFAULT_QUALITY = 3;
-	private static final String DEFAULT_ITEM = "DEFAULT_ITEM";
-	private static final int EXPIRED_SELLIN = -1;
-	private static final String AGED_BRIE = "Aged Brie";
+	private static final int POSITIVE_SELLIN_LESS_THAN_5 = 2;
+	private static final int SELLIN_BETWEEN_5_AND_10 = 7;
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	private static final int SELLIN_GREATER_THAN_10 = 15;
 	private static final int MAXIMUM_QUALITY = 50;
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final int EXPIRED_SELLIN = -2;
+	private static final String DEFAULT_ITEM = "DEFAULT_ITEM";
+	private static final int DEFAULT_QUALITY = 4;
+	private static final int NOT_EXPIRED_SELLIN = 16;
 
 	@Test
 	public void unexpiredDefaultItem_qualityDecreasesBy1() {
 		// GIVEN
 		GildedRose app = createGildedRoseWithOneItem(DEFAULT_ITEM, NOT_EXPIRED_SELLIN, DEFAULT_QUALITY);
 
-		// THEN
+		// WHEN
 		app.updateQuality();
 
 		// THEN
@@ -32,7 +36,7 @@ public class GildedRoseADefaultItemRefactoredTest {
 		// GIVEN
 		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, NOT_EXPIRED_SELLIN, DEFAULT_QUALITY);
 
-		// THEN
+		// WHEN
 		app.updateQuality();
 
 		// THEN
@@ -46,7 +50,7 @@ public class GildedRoseADefaultItemRefactoredTest {
 		// GIVEN
 		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, EXPIRED_SELLIN, DEFAULT_QUALITY);
 
-		// THEN
+		// WHEN
 		app.updateQuality();
 
 		// THEN
@@ -60,7 +64,7 @@ public class GildedRoseADefaultItemRefactoredTest {
 		// GIVEN
 		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, NOT_EXPIRED_SELLIN, MAXIMUM_QUALITY);
 
-		// THEN
+		// WHEN
 		app.updateQuality();
 
 		// THEN
@@ -74,11 +78,59 @@ public class GildedRoseADefaultItemRefactoredTest {
 		// GIVEN
 		GildedRose app = createGildedRoseWithOneItem(DEFAULT_ITEM, EXPIRED_SELLIN, DEFAULT_QUALITY);
 
-		// THEN
+		// WHEN
 		app.updateQuality();
 
 		// THEN
 		Item expected = new Item(DEFAULT_ITEM, EXPIRED_SELLIN - 1, DEFAULT_QUALITY - 2);
+
+		assertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void backStagePassesBeyond10Days_qualityIncreasesBy1() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES,
+				SELLIN_GREATER_THAN_10, DEFAULT_QUALITY);
+
+		// WHEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(BACKSTAGE_PASSES,
+				SELLIN_GREATER_THAN_10 - 1, DEFAULT_QUALITY + 1);
+
+		assertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void backStageBetween5And10Days_qualityIncreasesBy2() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES,
+				SELLIN_BETWEEN_5_AND_10, DEFAULT_QUALITY);
+
+		// WHEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(BACKSTAGE_PASSES,
+				SELLIN_BETWEEN_5_AND_10 - 1, DEFAULT_QUALITY + 2);
+
+		assertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void backStageLessThan5Days_qualityIncreasesBy3() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES,
+				POSITIVE_SELLIN_LESS_THAN_5, DEFAULT_QUALITY);
+
+		// WHEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(BACKSTAGE_PASSES,
+				POSITIVE_SELLIN_LESS_THAN_5 - 1, DEFAULT_QUALITY + 3);
 
 		assertItem(expected, app.items[0]);
 	}
