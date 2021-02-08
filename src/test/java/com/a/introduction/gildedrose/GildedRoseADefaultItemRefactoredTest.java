d9e6d893f8116995cdb76db55fb7cd9471144eb4
martos <martos@strands.com>
2021-02-08 16:09:16 +0100

    1. Clarity of code [Unit tests refactor - Gilded Rose] - Refactor Aged Brie tests (Test 1, 2 and 3)

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemRefactoredTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemRefactoredTest.java
@@ -10,6 +10,8 @@ public class GildedRoseADefaultItemRefactoredTest {
 	private static final int DEFAULT_QUALITY = 3;
 	private static final String DEFAULT_ITEM = "DEFAULT_ITEM";
 	private static final int EXPIRED_SELLIN = -1;
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final int MAXIMUM_QUALITY = 50;
 
 	@Test
 	public void unexpiredDefaultItem_qualityDecreasesBy1() {
@@ -21,6 +23,48 @@ public class GildedRoseADefaultItemRefactoredTest {
 
 		// THEN
 		Item expected = new Item(DEFAULT_ITEM, NOT_EXPIRED_SELLIN - 1, DEFAULT_QUALITY - 1);
+
+		assertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void unexpiredAgedBrie_qualityIncreasesBy1() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, NOT_EXPIRED_SELLIN, DEFAULT_QUALITY);
+
+		// THEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(AGED_BRIE, NOT_EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 1);
+
+		assertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void expiredAgedBrie_qualityIncreasesBy2() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, EXPIRED_SELLIN, DEFAULT_QUALITY);
+
+		// THEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(AGED_BRIE, EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 2);
+
+		assertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void unexpiredAgedBrie_qualityDoesNotGoBeyondMaximum() {
+		// GIVEN
+		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, NOT_EXPIRED_SELLIN, MAXIMUM_QUALITY);
+
+		// THEN
+		app.updateQuality();
+
+		// THEN
+		Item expected = new Item(AGED_BRIE, NOT_EXPIRED_SELLIN - 1, MAXIMUM_QUALITY);
 
 		assertItem(expected, app.items[0]);
 	}
@@ -38,6 +82,7 @@ public class GildedRoseADefaultItemRefactoredTest {
 
 		assertItem(expected, app.items[0]);
 	}
+
 
 	private GildedRose createGildedRoseWithOneItem(String defaultItem, int notExpiredSellin, int defaultQuality) {
 		Item item = new Item(defaultItem, notExpiredSellin, defaultQuality);
